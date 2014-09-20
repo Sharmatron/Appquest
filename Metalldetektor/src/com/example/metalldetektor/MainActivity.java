20531ae557a161056bc62d2a41f3253675daa442
Sharmelan Selvachandran <sharmelan.s@gmail.com>
2014-09-20 14:16:12 +0200

    2.

--- Metalldetektor/src/com/example/metalldetektor/MainActivity.java
+++ Metalldetektor/src/com/example/metalldetektor/MainActivity.java
@@ -6,8 +6,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
+//
+/**
+ * this is new 
+ * 
+ */
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
